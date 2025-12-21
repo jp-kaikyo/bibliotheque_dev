b57ff5389d1cbfc7afa6608d440904cd3eac3ba7
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-12-21 11:38:07 +0100

    V 1.5 : creation js calcule

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-//// variable ////
+///===================///
+/// ==== CALCULE ==== ///
+///===================///
 
 
-let jp = 0;
+//  recup les nombre  //
+let valeur_une = document.getElementById("A");
+let A = valeur_une.innerHTML;
+A = Number(A)
+
+let valeur_deux = document.getElementById("B");
+let B = valeur_deux.innerHTML;
+B = Number(B)
+
+let valeur_resulta = document.getElementById("AB");
+let AB = valeur_resulta.innerHTML;
+AB = Number(AB)
 
 
+//  addition et affichage  //
+AB = A + B
+valeur_resulta.innerHTML = AB
 
 
-//// Attendre que HTML et CSS soient chargés ////
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Page chargée !');
-
-    for (jp = 0; jp < 11; jp++) {
-        if (jp < 10) {
-        }else {
-         console.log("bravo !!")
-        }
-    }
-});
-
-
-// 
+//  débogueur  //
+console.log("valeur 1 :", A, "+")
+console.log("valeur 2 :", B, "=")
+console.log("resulta  :", AB)
